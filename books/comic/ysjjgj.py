8f040eeed486b79152ed49a0c8b70f243be56f49
yrq0118 <yrq930118@gmail.com>
2019-01-17 22:00:49 +0800

    Create ysjjgj.py

---
+++ books/comic/ysjjgj.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return ysjjgj
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]異世界建國記'
+    description         = u'原創作者： KOIZUMI, 櫻木櫻'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]異世界建國記', 'https://www.cartoonmad.com/comic/6049.html')]
