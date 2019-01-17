b09f300fd062444813ae44b01edbb0a1bf833047
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:05:48 +0800

    Update dzyz.py

--- books/comic/dzyz.py
+++ books/comic/dzyz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return dzyz
 
-class dzyz(CartoonMadBaseBook):
+class dzyz(ManHuaGuiBaseBook):
     title               = u'[漫画]盾之勇者成名錄'
     description         = u'原創作者： アネコユサギ 藍屋球'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class dzyz(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]盾之勇者成名錄', 'https://www.cartoonmad.com/comic/4605.html')]
+    feeds               = [(u'[漫画]盾之勇者成名錄', 'https://www.manhuagui.com/comic/16034/')]
