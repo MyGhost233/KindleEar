5c6b8942f72c196c6a5303f565ebbe1accddcfc9
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:08:16 +0800

    Update sxzyys.py

--- books/comic/sxzyys.py
+++ books/comic/sxzyys.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return sxzyys
 
-class sxzyys(CartoonMadBaseBook):
+class sxzyys(ManHuaGuiBaseBook):
     title               = u'[漫画]雙星之陰陽師'
     description         = u' 原創作者： 助野嘉昭'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class sxzyys(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]雙星之陰陽師', 'https://www.cartoonmad.com/comic/3376.html')]
+    feeds               = [(u'[漫画]雙星之陰陽師', 'https://www.manhuagui.com/comic/11015/')]
