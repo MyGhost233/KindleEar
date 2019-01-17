f0d8dcf4975a1abb2e8885659523a3ff3d755e40
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:10:51 +0800

    Update ysjkxq.py

--- books/comic/ysjkxq.py
+++ books/comic/ysjkxq.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return ysjkxq
 
-class ysjkxq(CartoonMadBaseBook):
+class ysjkxq(ManHuaGuiBaseBook):
     title               = u'[漫画]从死亡之旅开始的异世界狂想曲'
     description         = u'原創作者： あやめぐむ,愛七ひろ'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class ysjkxq(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]从死亡之旅开始的异世界狂想曲', 'https://www.cartoonmad.com/comic/4855.html')]
+    feeds               = [(u'[漫画]从死亡之旅开始的异世界狂想曲', 'https://www.manhuagui.com/comic/15495/')]
