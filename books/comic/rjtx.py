5a7f06029adeceeec058ca9b0008209ed6fd6078
yrq0118 <yrq930118@gmail.com>
2019-01-17 21:32:50 +0800

    Create rjtx.py

---
+++ books/comic/rjtx.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return rjtx
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]入间同学入魔了'
+    description         = u'原創作者： 西修'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]入间同学入魔了', 'https://www.cartoonmad.com/comic/5677.html')]
