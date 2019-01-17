b83da1918aa7318c3d1d3e921156db11c479013a
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:07:18 +0800

    Update sgw.py

--- books/comic/sgw.py
+++ books/comic/sgw.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return sgw
 
-class sgw(CartoonMadBaseBook):
+class sgw(ManHuaGuiBaseBook):
     title               = u'[漫画]失格纹的最强贤者'
     description         = u'原創作者： 肝匠, 馮昊, 進行諸島'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class sgw(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]失格纹的最强贤者', 'https://www.cartoonmad.com/comic/5993.html')]
+    feeds               = [(u'[漫画]失格纹的最强贤者', 'https://www.manhuagui.com/comic/25190/')]
