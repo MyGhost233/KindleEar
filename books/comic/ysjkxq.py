c3bc311ccac49e5ee87f9c9e48b9156763ae32ee
yrq0118 <yrq930118@gmail.com>
2019-01-17 22:01:40 +0800

    Create ysjkxq.py

---
+++ books/comic/ysjkxq.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return ysjkxq
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]从死亡之旅开始的异世界狂想曲'
+    description         = u'原創作者： あやめぐむ,愛七ひろ'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]从死亡之旅开始的异世界狂想曲', 'https://www.cartoonmad.com/comic/4855.html')]
