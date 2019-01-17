9523c471180dc8f95f919323a2a996d4fdda822d
yrq0118 <yrq930118@gmail.com>
2019-01-18 03:02:57 +0800

    Update and rename xzzs.py to swasz.py

--- books/comic/swasz.py
+++ books/comic/swasz.py
@@ -4,14 +4,14 @@
 from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
-    return OVERLORD
+    return swasz
 
 class OVERLORD(ManHuaGuiBaseBook):
-    title               = u'[漫画]OVERLORD'
+    title               = u'[漫画]身为暗杀者的我明显比勇者还强'
     description         = u'原創作者： 丸山くがね, 深山フギン'
     language            = 'zh-tw'
     feed_encoding       = 'big5'
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]OVERLORD', 'https://www.manhuagui.com/comic/15761/')]
+    feeds               = [(u'[漫画]身为暗杀者的我明显比勇者还强', 'https://www.manhuagui.com/comic/30367/')]
