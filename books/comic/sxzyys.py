3a1c947dfef0d66d149d9b98fca52f2eeeb2377c
yrq0118 <yrq930118@gmail.com>
2019-01-17 21:58:25 +0800

    Create sxzyys.py

---
+++ books/comic/sxzyys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return sxzyys
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]雙星之陰陽師'
+    description         = u' 原創作者： 助野嘉昭'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]雙星之陰陽師', 'https://www.cartoonmad.com/comic/3376.html')]
