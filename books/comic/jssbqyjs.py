7cd7748db372f3d2126d8e051546aa5b41b74ea0
yrq0118 <yrq930118@gmail.com>
2019-01-17 21:57:38 +0800

    Create jssbqyjs.py

---
+++ books/comic/jssbqyjs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return jssbqyjs
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]即使是不起眼劍聖亦是最強'
+    description         = u'原創作者： あっぺ,明石六郎'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]即使是不起眼劍聖亦是最強', 'https://www.cartoonmad.com/comic/7783.html')]
