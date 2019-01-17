082e0177f6344c2b97823a7a07d2dd869d267a43
yrq0118 <yrq930118@gmail.com>
2019-01-17 21:33:12 +0800

    Create tkqf.py

---
+++ books/comic/tkqf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return tkqf
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]天空侵犯'
+    description         = u'原創作者： 大羽隆廣,三浦追儺'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]天空侵犯', 'https://www.cartoonmad.com/comic/3477.html')]
