22ef6696c1eb89f71f2e2376b80799bc9c729e5f
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:08:48 +0800

    Update szzs.py

--- books/comic/szzs.py
+++ books/comic/szzs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return xzzs
 
-class xzzs(CartoonMadBaseBook):
+class xzzs(ManHuaGuiBaseBook):
     title               = u'[漫画]賢者之孫'
     description         = u'原創作者： 緒方俊輔, 吉岡剛'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class xzzs(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]賢者之孫', 'https://www.cartoonmad.com/comic/5331.html')]
+    feeds               = [(u'[漫画]賢者之孫', 'https://www.manhuagui.com/comic/20228/')]
