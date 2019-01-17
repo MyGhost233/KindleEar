50a69f6e148274a39854cfbe0cc77ee300623b04
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:04:33 +0800

    Update blbw.py

--- books/comic/blbw.py
+++ books/comic/blbw.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return blbw
 
-class blbw(CartoonMadBaseBook):
+class blbw(ManHuaGuiBaseBook):
     title               = u'[漫画]百炼霸王与圣约女武神'
     description         = u'原創作者： 鷹山誠一,chany'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class blbw(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]百炼霸王与圣约女武神', 'https://www.cartoonmad.com/comic/4820.html')]
+    feeds               = [(u'[漫画]百炼霸王与圣约女武神', 'https://www.manhuagui.com/comic/19060/')]
