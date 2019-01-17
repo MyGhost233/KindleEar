54174a246deca95c68c2aeddff18f5e84e39fdbf
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:05:22 +0800

    Update dyl.py

--- books/comic/dyl.py
+++ books/comic/dyl.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return dyl
 
-class dyl(CartoonMadBaseBook):
-    title               = u'[漫画]地獄樂'
+class dyl(ManHuaGuiBaseBook):
+    title               = u'[漫画]地狱乐'
     description         = u'原創作者： 緒方俊輔, 吉岡剛'
     language            = 'zh-tw'
     feed_encoding       = 'big5'
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]地獄樂', 'https://www.cartoonmad.com/comic/7882.html')]
+    feeds               = [(u'[漫画]地狱乐', 'https://www.manhuagui.com/comic/27739/')]
