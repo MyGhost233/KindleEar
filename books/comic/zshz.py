a4067098825ca3b3b0d788c69d2267e7d8666c1e
yrq0118 <yrq930118@gmail.com>
2019-01-17 22:00:06 +0800

    Create zshz.py

---
+++ books/comic/zshz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return zshz
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]咒術回戰'
+    description         = u'原創作者： 芥見下々'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]咒術回戰', 'https://www.cartoonmad.com/comic/7654.html')]
