fb4e11b710ab3fbf138cd53a3c622ff0d4b839c6
yrq0118 <yrq930118@gmail.com>
2019-01-17 21:32:11 +0800

    Create szzs.py

---
+++ books/comic/szzs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return xzzs
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]賢者之孫'
+    description         = u'原創作者： 緒方俊輔, 吉岡剛'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]賢者之孫', 'https://www.cartoonmad.com/comic/5331.html')]
