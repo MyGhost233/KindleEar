e0fed26777e77b3260ca8cceae0a2b646d7f97f4
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:06:19 +0800

    Update jssbqyjs.py

--- books/comic/jssbqyjs.py
+++ books/comic/jssbqyjs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return jssbqyjs
 
-class jssbqyjs(CartoonMadBaseBook):
+class jssbqyjs(ManHuaGuiBaseBook):
     title               = u'[漫画]即使是不起眼劍聖亦是最強'
     description         = u'原創作者： あっぺ,明石六郎'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class jssbqyjs(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]即使是不起眼劍聖亦是最強', 'https://www.cartoonmad.com/comic/7783.html')]
+    feeds               = [(u'[漫画]即使是不起眼劍聖亦是最強', 'https://www.manhuagui.com/comic/28261/')]
