42ca2483db2bccfdc1538750dc3812409fa0566b
yrq0118 <yrq930118@gmail.com>
2019-01-17 21:32:30 +0800

    Create slm.py

---
+++ books/comic/slm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author: insert0003 <https://github.com/insert0003>
+from .cartoonmadbase import CartoonMadBaseBook
+
+def getBook():
+    return slm
+
+class Hunter(CartoonMadBaseBook):
+    title               = u'[漫画]關於我轉生後成為史萊姆的那件事'
+    description         = u' 原創作者： 川上泰樹,伏瀨'
+    language            = 'zh-tw'
+    feed_encoding       = 'big5'
+    page_encoding       = 'big5'
+    mastheadfile        = 'mh_default.gif'
+    coverfile           = 'cv_bound.jpg'
+    feeds               = [(u'[漫画]關於我轉生後成為史萊姆的那件事', 'https://www.cartoonmad.com/comic/4387.html')]
