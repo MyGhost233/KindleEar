be9772e3f2441bf5e1a1113b007a1236c3083d1c
yrq0118 <yrq930118@gmail.com>
2019-01-18 02:12:09 +0800

    Update zshz.py

--- books/comic/zshz.py
+++ books/comic/zshz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #Author: insert0003 <https://github.com/insert0003>
-from .cartoonmadbase import CartoonMadBaseBook
+from .manhuaguibase import ManHuaGuiBaseBook
 
 def getBook():
     return zshz
 
-class zshz(CartoonMadBaseBook):
+class zshz(ManHuaGuiBaseBook):
     title               = u'[漫画]咒術回戰'
     description         = u'原創作者： 芥見下々'
     language            = 'zh-tw'
@@ -14,4 +14,4 @@ class zshz(CartoonMadBaseBook):
     page_encoding       = 'big5'
     mastheadfile        = 'mh_default.gif'
     coverfile           = 'cv_bound.jpg'
-    feeds               = [(u'[漫画]咒術回戰', 'https://www.cartoonmad.com/comic/7654.html')]
+    feeds               = [(u'[漫画]咒術回戰', 'https://www.manhuagui.com/comic/28004/')]
