e91b8bd6deb1277e41200f15b57473f8771a1bbb
yrq0118 <yrq930118@gmail.com>
2019-02-09 22:22:12 +0800

    Create Ruyixlj.py

---
+++ books/biquge/Ruyixlj.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return Ruyixlj
+
+
+class Ruyixlj(xxbiqugebase):
+    title = u'[小说]如意小郎君'
+    description = u'荣小荣'
+    feeds = 'https://www.xxbiquge.com/65_65306/'
