61d26a12f4e6197b7b276ab79839b02e02ebeef4
yrq0118 <yrq930118@gmail.com>
2019-02-09 22:01:58 +0800

    Create Ruyixlj.py

---
+++ books/cn3k5/Ruyixlj.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import cn3k5
+
+
+def getBook():
+    return Ruyixlj
+
+
+class Ruyixlj(cn3k5):
+    title = u'[小说]如意小郎君'
+    description = u'荣小荣'
+    feeds = 'http://www.cn3k5.com/10/10170/'
