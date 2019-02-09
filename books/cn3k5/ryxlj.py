23d64c9122e9804b814b3735530e568ac2a0eaef
yrq0118 <yrq930118@gmail.com>
2019-02-09 22:22:43 +0800

    Update ryxlj.py

--- books/cn3k5/ryxlj.py
+++ books/cn3k5/ryxlj.py
@@ -9,6 +9,6 @@ def getBook():
 
 
 class ryxlj(cn3k5):
-    title = u'[小说]如意小郎君'
-    description = u'荣小荣'
+    title = u'[小说]如意小郎君2'
+    description = u'荣小荣2'
     feeds = 'http://www.cn3k5.com/10/10170/'
