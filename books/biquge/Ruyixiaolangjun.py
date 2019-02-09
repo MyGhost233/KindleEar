134aadc64db0c86ec57729bcd9cde906bec28379
yrq0118 <yrq930118@gmail.com>
2019-02-09 21:37:03 +0800

    Create Ruyixiaolangjun.py

---
+++ books/biquge/Ruyixiaolangjun.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return Ruyixiaolangjun
+
+
+class Ruyixiaolangjun(xxbiqugebase):
+    title = u'[小说]如意小郎君'
+    description = u'荣小荣'
+feeds = 'https://www.xxbiquge.com/48_48247/'
