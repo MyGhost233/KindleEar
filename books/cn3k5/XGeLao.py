78ca5fb904bc4b1c4e3da83e559b39b291f4b458
我的鬼鬼 <yrq930118@gmail.com>
2020-04-09 16:21:24 +0800

    Create XGeLao.py

---
+++ books/cn3k5/XGeLao.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import cn3k5
+
+
+def getBook():
+    return XGeLao
+
+
+class XGeLao(cn3k5):
+    title = u'[小说]小阁老'
+    description = u'三戒大师'
+    feeds = 'http://www.cn3k5.com/99/99774/'
