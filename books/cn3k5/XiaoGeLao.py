4c402513fd5bb2642f9fd193534243b3880ee8e2
我的鬼鬼 <yrq930118@gmail.com>
2020-04-09 15:48:21 +0800

    Update XiaoGeLao.py

--- books/cn3k5/XiaoGeLao.py
+++ books/cn3k5/XiaoGeLao.py
@@ -6,10 +6,10 @@ from books.Novelbase import cn3k5
 
 
 def getBook():
-    return WenDing
+    return XiaoGeLao
 
 
-class WenDing(cn3k5):
+class XiaoGeLao(cn3k5):
     title = u'[小说]小阁老'
     description = u'三戒大师'
     feeds = 'http://www.cn3k5.com/99/99774/'
